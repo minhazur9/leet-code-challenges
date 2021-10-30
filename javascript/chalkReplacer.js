96159807e30056d8e6dc096fe06b250094168320
minhazur9 <minhazur9@gmail.com>
2021-10-30 01:34:40 -0400

    chalk replacer

---
+++ javascript/chalkReplacer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} chalk
+ * @param {number} k
+ * @return {number}
+ */
+const chalkReplacer = (chalk, k) => {
+  const maxStudents = chalk.length - 1;
+  let index = -1;
+  while (k >= 0) {
+    index++;
+    if (index > maxStudents) index = 0;
+    k -= chalk[index];
+  }
+  return index;
+};
+
+chalkReplacer([3, 4, 1, 2], 25);
